a58ff6db4c187f447c1d0491392d4852074efcf5
cboanta <cristina_boanta@proton.me>
2024-02-04 13:47:31 +0200

    implemented functionality for grabbing the jobs items containing the string "python"

--- jobs-scraper.py
+++ jobs-scraper.py
@@ -15,6 +15,14 @@ driver.get(jobs_listing_url)
 
 start = time.time()
 
+# Click the "accept cookies" button
+
+button_to_accept_cookies = driver.find_element(By.CLASS_NAME, "CookiesPopup__AcceptButton.eButton.eButton--Primary")
+
+# print(button_to_accept_cookies)
+
+button_to_accept_cookies.click()
+
 # will be used in the while loop
 initialScroll = 0
 finalScroll = 1000
@@ -30,7 +38,7 @@ while True:
 
 # we will stop the script for 3 seconds so that
 # the data can load
-    time.sleep(3)
+    time.sleep(1)
 # You can change it as per your needs and internet speed
 
     end = time.time()
@@ -39,3 +47,24 @@ while True:
 # You can change it as per your needs and internet speed
     if round(end - start) > 20:
         break
+
+
+
+# Extracting the data from the page and parsing it
+
+src = driver.page_source
+
+soup = BeautifulSoup(src, 'lxml')
+
+job_elements = driver.find_elements(By.CSS_SELECTOR, "h2.JCContentMiddle__Title a")
+
+# print(job_elements)
+
+for job_element in job_elements:
+    # print(type((job_element.text)))
+    if 'python' in job_element.get_attribute("textContent").lower():
+        job_link = job_element.get_attribute("href")
+        print(f'Link for this job: {job_link}')
+    else:
+        # print('This is not a python job')
+        pass
